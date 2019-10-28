f312f0ec4b2119d397907a0bbea73afd851ada98
Tabebee <tabibone@gmail.com>
2019-10-28 14:24:28 -0400

    Updates

---
+++ precache-manifest.44644b5d9abe766bb7611c23b91f9365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88135bdeb8aa9230040eb527bbb91754",
+    "url": "/movie-dictionary/index.html"
+  },
+  {
+    "revision": "c1085b773829c3b1c571",
+    "url": "/movie-dictionary/static/css/main.e1b113c4.chunk.css"
+  },
+  {
+    "revision": "c0cfed8ea05c68a19f2b",
+    "url": "/movie-dictionary/static/js/2.e7d1edc7.chunk.js"
+  },
+  {
+    "revision": "c1085b773829c3b1c571",
+    "url": "/movie-dictionary/static/js/main.c87ffe46.chunk.js"
+  },
+  {
+    "revision": "d22b34c066fae9804c76",
+    "url": "/movie-dictionary/static/js/runtime-main.d072a0ea.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/movie-dictionary/static/media/logo.25bf045c.svg"
+  }
+]);
